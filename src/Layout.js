51efef7ec60c35eadab346dde7676ef6731d3d3e
unknown <shane.palomo.24@gmail.com>
2020-04-19 21:37:06 +0800

    Fix navbar

--- src/Layout.js
+++ src/Layout.js
@@ -10,9 +10,9 @@ const gridTable = () => {
         <div id="fullpage" className='container-fluid'>
                 <MDBContainer fluid='True'>
                     <MDBRow>
-                    <MDBCol md="6" id="first" >1</MDBCol>
-                    <MDBCol md="4" >2</MDBCol>
-                    <MDBCol md="2" >3</MDBCol>
+                    <MDBCol sm="6" id="first" >1</MDBCol>
+                    <MDBCol sm="4" >2</MDBCol>
+                    <MDBCol sm="2" >3</MDBCol>
                 </MDBRow>
             </MDBContainer>
         </div>
@@ -22,17 +22,19 @@ const gridTable = () => {
 
 const navigationBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="navBarTop">
-            <a class="navbar-brand" href="#" id="headerFont">Learning by Doing</a>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item active"><a className="nav-link" href="#">About</a></li>
-                    <li className="nav-item "><a className="nav-link" href="#">Portfolio</a></li>
-                    <li className="nav-item "><a className="nav-link"  href="#">Games</a></li>
-                    <li className="nav-item "><a className="nav-link"  href="#">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
+        <div className = "container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="navBarTop">
+                <a class="navbar-brand" href="#" id="headerFont">Learning by Doing</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active"><a className="nav-link" href="#">About</a></li>
+                        <li className="nav-item "><a className="nav-link" href="#">Portfolio</a></li>
+                        <li className="nav-item "><a className="nav-link"  href="#">Games</a></li>
+                        <li className="nav-item "><a className="nav-link"  href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     );
 }
 
