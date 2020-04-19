e1e0e2745d281178514443dd15c2385f5221fea8
Chichigo123 <shanepalomo@gmail.com>
2020-04-19 06:11:12 +0000

    Add TicTacToe source

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './TicTacToe'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> This is a game imported from a tutorial in 
+            <a href="https://reactjs.org/docs/create-a-new-react-app.html">reactjs.org</a>
+        </h1>
+        <Game />
+      </div>
+);
+  }
 }
-
 export default App;
