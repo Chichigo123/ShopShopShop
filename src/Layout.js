2c9834f53d4e498a847e89e831edb9a7ecfc22a4
unknown <shane.palomo.24@gmail.com>
2020-04-19 22:02:36 +0800

    Fix containers

--- src/Layout.js
+++ src/Layout.js
@@ -7,44 +7,44 @@ import './Layout.css'
 
 const gridTable = () => {
     return (
-        <div id="fullpage" className='container-fluid'>
-                <MDBContainer fluid='True'>
-                    <MDBRow>
-                    <MDBCol sm="6" id="first" >1</MDBCol>
-                    <MDBCol sm="4" >2</MDBCol>
-                    <MDBCol sm="2" >3</MDBCol>
-                </MDBRow>
-            </MDBContainer>
-        </div>
+      
+        <MDBContainer fluid='True' id="fullpage">
+            <MDBRow>
+                <MDBCol sm="6" id="first" >1</MDBCol>
+                <MDBCol sm="4" >2</MDBCol>
+                <MDBCol sm="2" >3</MDBCol>
+            </MDBRow>
+        </MDBContainer>
+    
     );
 
 }
 
 const navigationBar = () => {
     return (
-        <div className = "container">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="navBarTop">
-                <a class="navbar-brand" href="#" id="headerFont">Learning by Doing</a>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active"><a className="nav-link" href="#">About</a></li>
-                        <li className="nav-item "><a className="nav-link" href="#">Portfolio</a></li>
-                        <li className="nav-item "><a className="nav-link"  href="#">Games</a></li>
-                        <li className="nav-item "><a className="nav-link"  href="#">Contact</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="navBarTop">
+            <a class="navbar-brand" href="#" id="headerFont">Learning by Doing</a>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active"><a className="nav-link" href="#">About</a></li>
+                    <li className="nav-item "><a className="nav-link" href="#">Portfolio</a></li>
+                    <li className="nav-item "><a className="nav-link"  href="#">Games</a></li>
+                    <li className="nav-item "><a className="nav-link"  href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
     );
 }
 
 
 const Layout = () => {
    return (       
-            <div>
+        <MDBContainer fluid='True' id="fullpage">
+            <MDBRow>
                 {navigationBar()}
-                {gridTable()}
-            </div>
+            </MDBRow>
+            {gridTable()}
+        </MDBContainer>
     
           
    );
