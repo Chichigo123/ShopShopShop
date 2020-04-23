713523281cb61f00fafb74ade21645e698f34934
unknown <shane.palomo.24@gmail.com>
2020-04-24 00:16:24 +0800

    Add background and some ingormation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Media.css';
 import Game from './TicTacToe'
 import Layout from './Layout'
 
--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import * as serviceWorker from './serviceWorker';
 
 
 //CSS
-import './index.css';
+
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js'
+import './index.css';
 
 
 // ReactDOM.render(
