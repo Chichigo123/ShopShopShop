8ae8ac10c162734b2e48c6c759439858c8b31525
unknown <shane.palomo.24@gmail.com>
2020-05-02 16:43:49 +0800

    ADd welcome

--- src/Home.js
+++ src/Home.js
@@ -8,17 +8,17 @@ const gridTable = () => {
         // className="w-50"
             <div className="home">
                 <div id="home-left">
-                    {/* <img src="sunset.jpg" alt="beach" ></img> */}
-                    {/* <div>
+                    <img src="sunset.jpg" alt="beach" ></img>
+                    <div>
                         <h1 className="welcomeHeader">Welcome</h1>  
                         <h5 className="welcomeHeader" style={{"color":"#6C4F3D", "opacity":".5"}}>
                             This is an experimental site for front-end and back-end web development.<br/>
                         </h5>
-                    </div> */}
+                    </div>
                 </div>
 
-                {/* <div id="home-right">  
-                    <div className="aboutPage">
+                <div id="home-right">  
+                    {/* <div className="aboutPage">
                         <p> About Site</p>
                     </div>
                     <div className="aboutPageList">
@@ -51,8 +51,8 @@ const gridTable = () => {
                             <li className="fa fa-envelope"></li>
                             <p>shanepalomo@gmail.com</p>                           
                         </ul>
-                    </div>
-                </div> */}
+                    </div> */}
+                </div>
             </div>
           
     );
